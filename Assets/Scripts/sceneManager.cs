d71a8ffd170bdf43c24a916929587fc73addefe6
ChillBeanJosh <joshover23@gmail.com>
2026-09-10 22:30:21 -0400

    Unity Import

---
+++ Assets/Scripts/sceneManager.cs
@@ -0,0 +1,63 @@
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneManager : MonoBehaviour
+{
+    public static sceneManager Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    //List of Scenes in the build Index, make sure they are in ORDER.
+    public enum Scene
+    {
+        MainMenu,
+        Gameplay_1,
+        Gameplay_2,
+
+
+    }
+
+    //List of Scene Functions.
+
+    //Used to refrence any specific scene on the build index.
+    public void LoadScene(Scene scene)
+    {
+        SceneManager.LoadScene(scene.ToString());
+    }
+
+
+    //Used for the Start Game Button.
+    public void LoadNewGame1()
+    {
+        SceneManager.LoadScene(Scene.Gameplay_1.ToString());
+    }
+
+    public void LoadNewGame2()
+    {
+        SceneManager.LoadScene(Scene.Gameplay_2.ToString());
+    }
+
+
+    //Loads next Scene in build index.
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+
+    //Loads the Main Menu Scene.
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(Scene.MainMenu.ToString());
+    }
+
+    //Restarts the current Scene.
+    public void RestartScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
